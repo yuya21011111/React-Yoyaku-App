1a41478d24529c6d1f24be9cd468d42ab41acb27
YuyaYamazi <kaimomo12345@gmail.com>
2023-12-02 04:09:35 +0900

    antd導入

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import { Form } from "antd"
+import { Link } from "react-router-dom"
+
+const onFinish = (values) => {
+  // values.preventDefault()
+  console.log(values)
+}
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className="mt-40 mx-20">
+      <div className="flex justify-center mt-32 mx-10 mb-10 shadow-lg bg-gray-100 px-8">
+        <Form className="w-full max-w-2xl" onFinish={onFinish}>
+          <p className="text-2xl text-black font-bold text-center mb-5 mt-4">
+            ログイン
+          </p>
+        
+          <div className="md:flex md:items-center mb-6">
+            <Form.Item label="メールアドレス" name="email" className="md:w-1/3">
+            <input
+                className="bg-gray-200 appearance-none border-2 border-gray-400 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                type="email"
+                placeholder="aaa@aaa.com"
+              />
+            </Form.Item>
+          </div>
+
+          <div className="md:flex md:items-center mb-6">
+            <Form.Item label="パスワード" name="password" className="md:w-1/3">
+            <input
+                className="bg-gray-200 appearance-none border-2 border-gray-400 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                type="text"
+                placeholder="********"
+              />
+            </Form.Item>
+          </div>
+        <div className='flex justify-end'>
+            <button type='submit' className='mb-4 border border-cyan-400 bg-cyan-400 rounded-lg text-white py-2 px-4'>ログインする</button>
+        </div>
+        <Link className='text-blue-400 text-sm' to="/Register">
+              新規登録はこちらから<storage className=" text-blue-500 font-medium">新規登録</storage>
+        </Link>
+        </Form>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,80 +1,56 @@
 import React from 'react'
-
+import { Form } from "antd"
+import { Link } from "react-router-dom"
+ 
 function Register() {
+
+  const onFinish = (values) => {
+    // values.preventDefault()
+    console.log(values)
+  }
     return (
     <div className="mt-40 mx-20">
-      <div className="flex justify-center mt-32 mx-10 mb-10">
-        <form className="w-full max-w-2xl">
-          <p className="text-2xl text-black font-bold text-center mb-5">
+      <div className="flex justify-center mt-32 mx-10 mb-10 shadow-lg bg-gray-100 px-8">
+        <Form className="w-full max-w-2xl" onFinish={onFinish}>
+          <p className="text-2xl text-black font-bold text-center mb-5 mt-4">
             新規登録フォーム
           </p>
           <div className="md:flex md:items-center mb-6">
-            <div className="md:w-1/3">
-              <label
-                className="block text-black font-bold md:text-right mb-1 md:mb-0 pr-4"
-                htmlFor="inline-first-name"
-              >
-                名前
-                <text className="text-white bg-red-500 font-normal text-sm ml-2 p-0.5 rounded-md">
-                  必須
-                </text>
-              </label>
-            </div>
-
-            <div className="md:w-2/3">
-              <input
+            <Form.Item label="名前" name="name" className="md:w-1/3">
+            <input
                 className="bg-gray-200 appearance-none border-2 border-gray-400 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                 type="text"
-                defaultValue=""
                 placeholder="山田　太郎"
-               
               />
-            </div>
+            </Form.Item>
           </div>
 
           <div className="md:flex md:items-center mb-6">
-            <div className="md:w-1/3">
-              <label className="block text-black font-bold md:text-right mb-1 md:mb-0 pr-4">
-                メールアドレス
-                <text className="text-white bg-red-500 font-normal text-sm ml-2 p-0.5 rounded-md">
-                  必須
-                </text>
-              </label>
-            </div>
-            <div className="md:w-2/3">
-              <input
+            <Form.Item label="メールアドレス" name="email" className="md:w-1/3">
+            <input
                 className="bg-gray-200 appearance-none border-2 border-gray-400 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                 type="email"
-                defaultValue=""
-                placeholder="例) example@gmail.me"
-               
+                placeholder="aaa@aaa.com"
               />
-            </div>
+            </Form.Item>
           </div>
 
           <div className="md:flex md:items-center mb-6">
-            <div className="md:w-1/3">
-              <label className="block text-black font-bold md:text-right mb-1 md:mb-0 pr-4">
-            　パスワード
-                <text className="text-white bg-red-500 font-normal text-sm ml-2 p-0.5 rounded-md">
-                  必須
-                </text>
-              </label>
-            </div>
-            <div className="md:w-2/3">
-              <input
+            <Form.Item label="パスワード" name="password" className="md:w-1/3">
+            <input
                 className="bg-gray-200 appearance-none border-2 border-gray-400 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                 type="text"
-                defaultValue=""
-                placeholder="例) adsffkkwe3432kge"
-               
+                placeholder="********"
               />
-            </div>
+            </Form.Item>
           </div>
         <div className='flex justify-end'>
-            <button type='button' className='border border-cyan-400 bg-cyan-400 rounded-lg text-white py-2 px-4'>登録する</button>
+            <button type='submit' className='mb-4 border border-cyan-400 bg-cyan-400 rounded-lg text-white py-2 px-4'>登録する</button>
         </div>
-        </form>
+        <Link className='text-blue-400 text-sm' to="/login">
+              既に登録済みの方ですか？ <storage className=" text-blue-500 font-medium">ログイン</storage>
+        </Link>
+        </Form>
       </div>
     </div>
     )
