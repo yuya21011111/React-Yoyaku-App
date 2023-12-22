538be2d82e8c807f10043711b39b61a8b23179be
YuyaYamazi <kaimomo12345@gmail.com>
2023-12-22 15:31:17 +0900

    Show

--- src/pages/Show/Show.jsx
+++ src/pages/Show/Show.jsx
@@ -14,11 +14,9 @@ function Show() {
         try {
             dispatch(ShowLoader(true))
             const response = await GetStoreId(id)
-            if(response.success)
-            {
+            if (response.success) {
                 setStore(response.data)
-            }else
-            {
+            } else {
                 message.error(response.message)
             }
             dispatch(ShowLoader(false))
@@ -30,15 +28,45 @@ function Show() {
 
     useEffect(() => {
         getData()
-    },[id])
-  return (
-   store && ( <div className='mt-4 bg-white p-8'>
-        <h1 className='text-green-500 text-2xl'>
-            {store.firstName} {store.lastName}
-        </h1>
-    </div>
-  )
-  )
+    }, [id])
+    return (
+        store && (<div className='mt-4 bg-white p-8'>
+            <h1 className='text-gray-500 text-2xl border-b-2 border-gray-300'>
+                <b>{store.firstName} {store.lastName}</b>
+            </h1>
+            <div className='flex flex-col gap-1 bg-white'>
+                <div className='flex justify-between'>
+                    <h4>
+                        <b>
+                            Gender:
+                        </b>
+                    </h4>
+                    <h4 className='text-gray-700 font-medium text-2xl'>{store.gender}</h4>
+                </div>
+            </div>
+            <div className='flex flex-col gap-1 bg-white p-1'>
+                <div className='flex justify-between'>
+                    <h4>
+                        <b>
+                            Email:
+                        </b>
+                    </h4>
+                    <h4 className='text-gray-700 font-medium text-2xl'>{store.email}</h4>
+                </div>
+                <div className='flex flex-col gap-1 bg-white p-1 border-b-2 border-gray-300'>
+                    <div className='flex justify-between'>
+                        <h4>
+                            <b>
+                             Phone:
+                            </b>
+                        </h4>
+                        <h4 className='text-gray-700 font-medium text-2xl'>{store.phone}</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+        )
+    )
 }
 
 export default Show
